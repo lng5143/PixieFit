a3d4582c0b5a6ef6e7abdcd8c06ddef88d2fed4d
lng5143 <nguyenphuonglam96@gmail.com>
2024-01-31 22:13:35 +0700

    InitColorArray, InitEnergyArray
    Signed-off-by: lng5143 <nguyenphuonglam96@gmail.com>

--- src/Services/Core/Managers/Resizer.cs
+++ src/Services/Core/Managers/Resizer.cs
@@ -4,9 +4,11 @@ using System.Reflection;
 
 namespace PixieFit.Core.Managers;
 
-public class Resizer 
+public class Resizer
 {
     public Bitmap picture { get; set; }
+    private double[][] energy { get; set; }
+    private long[][] color { get; set; }
 
     public Resizer()
     {
@@ -17,31 +19,85 @@ public class Resizer
     {
         MemoryStream stream = new MemoryStream(request.PhotoBytes);
         picture = new Bitmap(stream);
-        
+
+        InitColorArray(Width(), Height());
+        InitEnergyArray(Width(), Height());
+
         return null;
     }
 
     private void InitEnergyArray(int width, int height)
     {
-        
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                energy[i][j] = Energy(i, j);
+            }
+        }
     }
 
     private void InitColorArray(int width, int height)
     {
-
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                color[i][j] = picture.GetPixel(i, j).ToArgb();
+            }
+        }
     }
 
     // width of current picture
     private int Width()
     {
-        return 0;
+        return picture.Width;
     }
 
     // height of current picture 
     private int Height()
     {
-        return 0;
+        return picture.Height;
     }
+
+    private double Energy(int x, int y)
+    {
+        if (
+                x == 0 // outer left col
+                || x == Width() - 1 // outer right col
+                || y == 0 // top row
+                || y == Height() - 1 // bottom row
+        )
+        {
+            return 1000;
+        }
+        else
+        {
+            long leftARGB = color[x - 1][y];
+            long rightARGB = color[x + 1][y];
+            long upARGB = color[x][y - 1];
+            long downARGB = color[x][y + 1];
+
+            double xDiffSquared = CalculateColorDiffSquared(leftARGB, rightARGB);
+            double yDiffSquared = CalculateColorDiffSquared(upARGB, downARGB);
+
+            return Math.Sqrt(xDiffSquared + yDiffSquared);
+        }
+    }
+
+    private double CalculateColorDiffSquared(long argb1, long argb2)
+    {
+        var r1 = Convert.ToInt16((argb1 >> 16) & 0xFF);
+        var g1 = Convert.ToInt16((argb1 >> 8) & 0xFF);
+        int b1 = Convert.ToInt16(argb1 & 0xFF);
+
+        var r2 = Convert.ToInt16((argb2 >> 16) & 0xFF);
+        var g2 = Convert.ToInt16((argb2 >> 8) & 0xFF);
+        int b2 = Convert.ToInt16(argb2 & 0xFF);
+
+        return Math.Pow(r1 - r2, 2) + Math.Pow(g1 - g2, 2) + Math.Pow(b1 - b2, 2);
+    }
+    
 }
 
 public class DirectedEdge2D 

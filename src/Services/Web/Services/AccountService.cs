47c8e26c609837d7e27513a0c9095310b55f2fdc
lng5143 <nguyenphuonglam96@gmail.com>
2024-02-18 22:29:07 +0700

    added AccountService

--- src/Services/Web/Services/AccountService.cs
+++ src/Services/Web/Services/AccountService.cs
@@ -1,6 +1,13 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using PixieFit.Web.Business;
 using PixieFit.Web.Business.Models;
+using PixieFit.Web.Business.Entities;
+using Isopoh.Cryptography.Argon2;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http.HttpResults;
+using PixieFit.Web.Business.Enums;
 
 namespace PixieFit.Web.Services;
 
@@ -20,6 +27,61 @@ public class AccountService
 
     public async Task CreateAccount(SignUpRequest request)
     {
+        try
+        {
+            // validation 
+            if (string.IsNullOrEmpty(request.Email))
+                throw new Exception(ErrorCode.EMAIL_REQUIRED.ToString());
 
+            if (string.IsNullOrEmpty(request.Password))
+                throw new Exception(ErrorCode.PASSWORD_REQUIRED.ToString());
+
+            if (string.IsNullOrEmpty(request.Username))
+                throw new Exception(ErrorCode.USERNAME_REQUIRED.ToString());
+
+            // hashing and salting password 
+            var salt = RandomNumberGenerator.GetBytes(16).ToB64String();
+            var passwordHash = Argon2.Hash(request.Password, salt);
+
+            var user = new User
+            {
+                Email = request.Email,
+                Username = request.Username,
+                PasswordHash = passwordHash,
+                Salt = salt,
+                CreditAmount = 0
+            };
+
+            await _dbContext.Users.AddAsync(user);
+            // await _dbContext.SaveChangesAsync();
+        }
+        catch (Exception e)
+        {
+            throw new Exception(e.Message);
+        }
+    }
+
+    public async Task Login(LoginRequest request)
+    {
+        if (string.IsNullOrEmpty(request.Email))
+            throw new Exception(ErrorCode.EMAIL_REQUIRED.ToString());
+        
+        if (string.IsNullOrEmpty(request.Password))
+            throw new Exception(ErrorCode.PASSWORD_REQUIRED.ToString());
+
+        var user = await _dbContext.Users.FirstOrDefaultAsync(u => u.Email == request.Email);
+        
+        if (user == null)
+            throw new Exception("User not found");
+
+        var hashToVerify = Argon2.Hash(request.Password, user.Salt);
+        if (Argon2.Verify(hashToVerify, user.PasswordHash))
+        {
+            // TODO: create JWT token
+        }
+        else
+        {
+            throw new Exception("Invalid password");
+        }
     }
 }

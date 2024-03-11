3d659f36a1971066b0c442d5349c3a0f8c911a93
lng5143 <nguyenphuonglam96@gmail.com>
2024-03-11 22:17:30 +0700

    added tables

--- src/Services/Web/Business/PFContext.cs
+++ src/Services/Web/Business/PFContext.cs
@@ -10,6 +10,8 @@ public class PFContext : DbContext
     public DbSet<CreditPackage> CreditPackages { get; set; }
     public DbSet<Resize> Resizes { get; set; }
     public DbSet<UserTransaction> UserTransactions { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<Webhook> Webhooks { get; set; }
 
     public const string ConnectionString = 
         "Host=localhost; Port=5433; Database=PixieFit; Username=postgres; Password=5143322";
---
+++ src/Services/Web/Infrastructure/Migrations/20240311151647_AddWebhookOrder.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Web.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddWebhookOrder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    TotalAmount = table.Column<long>(type: "bigint", nullable: false),
+                    PaymentMethod = table.Column<int>(type: "integer", nullable: false),
+                    PaymentStatus = table.Column<int>(type: "integer", nullable: false),
+                    CreditPackageId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Webhooks",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    WebhookType = table.Column<int>(type: "integer", nullable: false),
+                    IntegrationPartner = table.Column<int>(type: "integer", nullable: false),
+                    DataJson = table.Column<string>(type: "text", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Webhooks", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Webhooks");
+        }
+    }
+}

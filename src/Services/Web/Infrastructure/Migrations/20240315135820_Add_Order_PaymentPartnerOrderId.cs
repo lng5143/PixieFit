ef21e7c92e8ac35ab69bf47f7b0a4f7fbe78d385
lng5143 <nguyenphuonglam96@gmail.com>
2024-03-15 20:59:31 +0700

    added migration Order_PaymentPartnerOrderId

---
+++ src/Services/Web/Infrastructure/Migrations/20240315135820_Add_Order_PaymentPartnerOrderId.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Web.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOrderPaymentPartnerOrderId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ResizedHeight",
+                table: "Resizes");
+
+            migrationBuilder.DropColumn(
+                name: "ResizedWidth",
+                table: "Resizes");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserId",
+                table: "Resizes",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FileName",
+                table: "Resizes",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<double>(
+                name: "ResizeHeight",
+                table: "Resizes",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "ResizeWidth",
+                table: "Resizes",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "TotalAmount",
+                table: "Orders",
+                type: "numeric",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentPartnerOrderId",
+                table: "Orders",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ResizeHeight",
+                table: "Resizes");
+
+            migrationBuilder.DropColumn(
+                name: "ResizeWidth",
+                table: "Resizes");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentPartnerOrderId",
+                table: "Orders");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Resizes",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FileName",
+                table: "Resizes",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ResizedHeight",
+                table: "Resizes",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ResizedWidth",
+                table: "Resizes",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "TotalAmount",
+                table: "Orders",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+        }
+    }
+}
